4c9bddd50cad6cdeff3734c6600f3edddd2d40e9
leylaarustemli <tu7h94maj@code.edu.az>
2024-04-26 00:33:12 +0400

    first commit

---
+++ scrip.js
@@ -0,0 +1,5 @@
+const input=document.getElementById("input")
+const button=document.getElementById("button")
+const footerCard=document.getElementById("footer-card")
+
+fetch(url).then(response=>response.json()).then(data=>{}).catch(error=>{})
